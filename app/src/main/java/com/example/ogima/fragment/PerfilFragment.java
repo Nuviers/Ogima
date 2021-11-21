ae58058f1863bf4525d4efa1428ac449f513fc51
Rafae Benedet <rafabenedetfer@gmail.com>
2021-11-21 15:04:17 -0300

    Carregando foto de perfil e fundo de perfil, verificar possiveis erros se tiver nulo ou se pega dados de outro usuario, usar um compressor de imagem e redimensionar elas ao setar ou algo do genero

--- app/src/main/java/com/example/ogima/fragment/PerfilFragment.java
+++ app/src/main/java/com/example/ogima/fragment/PerfilFragment.java
@@ -5,18 +5,22 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.ogima.R;
 import com.example.ogima.activity.EditarPerfilActivity;
+import com.example.ogima.helper.Base64Custom;
 import com.example.ogima.helper.ConfiguracaoFirebase;
 import com.example.ogima.helper.UsuarioFirebase;
 import com.example.ogima.model.Usuario;
@@ -28,7 +32,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -42,11 +51,29 @@ public class PerfilFragment extends Fragment {
     private Button buttonEditarPerfil;
     private Usuario usuario;
 
+    private String minhaFoto;
+    private String meuFundo;
+    private String apelido;
+
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDataBase();
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
 
     public PerfilFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        try{
+            testandoLog();
+            }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -83,6 +110,9 @@ public class PerfilFragment extends Fragment {
         textTeste = view.findViewById(R.id.textTeste);
 
         buttonEditarPerfil = view.findViewById(R.id.buttonEditarPerfil);
+
+        //Capturando foto de perfil e fundo de perfil do usuario
+        //testandoLog();
 
 
         //Recuperando imagem do usuário e fundo tambem
@@ -124,6 +154,67 @@ public class PerfilFragment extends Fragment {
 
 
         return view;
+    }
+
+    public void testandoLog(){
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child(idUsuario);
+
+
+        usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.getValue() != null){
+
+                    Usuario usuario = snapshot.getValue(Usuario.class);
+                    Log.i("FIREBASE", usuario.getIdUsuario());
+                    Log.i("FIREBASEA", usuario.getNomeUsuario());
+                    apelido = usuario.getApelidoUsuario();
+                    meuFundo = usuario.getMeuFundo();
+                    minhaFoto = usuario.getMinhaFoto();
+
+                    if(apelido != null){
+
+                        Toast.makeText(getActivity(), " Okay", Toast.LENGTH_SHORT).show();
+
+                        if(minhaFoto != null){
+                            Picasso.get().load(minhaFoto).into(imgFotoUsuario);
+                            Log.i("IMAGEM", "Sucesso ao atualizar foto de perfil");
+                        }else{
+                            Log.i("IMAGEM", "Falha ao atualizar foto de perfil");
+                        }
+
+                        if(meuFundo != null){
+                            Picasso.get().load(meuFundo).into(imgFundoUsuario);
+                            Log.i("IMAGEM", "Sucesso ao atualizar fundo de perfil");
+                        }else{
+                            Log.i("IMAGEM", "Falha ao atualizar fundo de perfil");
+                        }
+
+                    }else if(snapshot == null) {
+
+                        Toast.makeText(getActivity(), " Conta falta ser cadastrada", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                }else{
+                    Toast.makeText(getActivity(), "Por favor termine seu cadastro", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(getActivity(), "Cancelado", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
 }
