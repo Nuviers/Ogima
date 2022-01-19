dda266f8d9144de24b58390b93ecf000b49a16e2
Rafae Benedet <rafabenedetfer@gmail.com>
2022-01-19 01:39:15 -0300

    Problema com o tamanho da scrollview resolvido, arrumar o editarperfil que tá feio, e os icones da toolbar tbm. E os 3 pontinho também a cor.

--- app/src/main/java/com/example/ogima/ui/cadastro/IdadePessoas.java
+++ app/src/main/java/com/example/ogima/ui/cadastro/IdadePessoas.java
@@ -1,13 +1,10 @@
 package com.example.ogima.ui.cadastro;
 
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,12 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.ogima.R;
 import com.example.ogima.model.Usuario;
 
-import java.text.DateFormat;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 import java.time.format.ResolverStyle;
 import java.util.Locale;
 
@@ -43,7 +38,7 @@ public class IdadePessoas extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.idade_pessoa);
+        setContentView(R.layout.cad_nascimento);
 
         btnContinuarIdade = findViewById(R.id.btnContinuarIdade);
         edt_AnoNascimento = findViewById(R.id.edt_AnoNascimento);
