5dc1d0982bed04813400d72efb9fbfea72080f8c
Rafae Benedet <rafabenedetfer@gmail.com>
2022-01-31 15:05:21 -0300

    Mudanças feitas nos layout do fragment perfil e atualizando os dados no PerfilFragment.

--- app/src/main/java/com/example/ogima/fragment/PerfilFragment.java
+++ app/src/main/java/com/example/ogima/fragment/PerfilFragment.java
@@ -2,11 +2,9 @@ package com.example.ogima.fragment;
 
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
@@ -17,14 +15,9 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 import com.example.ogima.R;
 import com.example.ogima.activity.EditarPerfilActivity;
 import com.example.ogima.helper.Base64Custom;
@@ -46,7 +39,8 @@ import com.google.firebase.database.ValueEventListener;
  */
 public class PerfilFragment extends Fragment {
 
-    private TextView txtDeslogar, nickUsuario;
+    private TextView txtDeslogar, nickUsuario,
+            txtSeguidores, txtSeguindo, txtAmigos;
     private GoogleSignInClient mSignInClient;
     private ImageView imgFotoUsuario, imgFundoUsuario, imageViewGif, imageBorda;
 
@@ -63,6 +57,7 @@ public class PerfilFragment extends Fragment {
     private String emailUsuario, idUsuario;
     private DatabaseReference usuarioRef, usuarioRefs;
     private String exibirApelido;
+    private int seguidores, seguindo, amigos;
 
     private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDataBase();
     private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
@@ -101,6 +96,9 @@ public class PerfilFragment extends Fragment {
         nickUsuario = view.findViewById(R.id.textNickUsuario);
         imageButtonEditar = view.findViewById(R.id.imageButtonEditar);
         shimmerFrameLayout = view.findViewById(R.id.shimmer);
+        txtSeguidores = view.findViewById(R.id.textSeguidores);
+        txtSeguindo = view.findViewById(R.id.textSeguindo);
+        txtAmigos = view.findViewById(R.id.textAmigos);
         //view18 = view.findViewById(R.id.view18);
 
 
@@ -149,10 +147,17 @@ public class PerfilFragment extends Fragment {
                     exibirApelido = usuario.getExibirApelido();
                     epilepsia = usuario.getEpilepsia();
 
+                    String amigos = String.valueOf( usuario.getAmigosUsuario() );
+                    String seguindo = String.valueOf( usuario.getSeguindoUsuario() );
+                    String seguidores = String.valueOf( usuario.getSeguidoresUsuario() );
+
                     if (emailUsuario != null) {
-                        //Toast.makeText(getActivity(), " Okay", Toast.LENGTH_SHORT).show();
                         try {
-                            //nickUsuario.setText(nome);
+
+                            txtSeguidores.setText( seguidores );
+                            txtAmigos.setText( amigos );
+                            txtSeguindo.setText( seguindo );
+
                             if (minhaFoto != null) {
                                 if (epilepsia.equals("Sim")) {
                                     GlideCustomizado.montarGlideEpilepsia(getActivity(), minhaFoto, imageBorda, R.drawable.testewomamtwo);
