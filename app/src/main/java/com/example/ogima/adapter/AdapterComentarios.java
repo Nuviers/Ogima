b9ea00e0a7689289ca8adead9f4516d780a5b2f7
Rafae Benedet <rafabenedetfer@gmail.com>
2022-05-11 21:33:00 -0300

    - Excluir o próprio comentário está funcionando, verificar se a lista está se adequando de forma correta ao excluir comentário.
    - Falta o icone para denunciar o comentário e (ocultar o comentário - caso eu seja o dono da postagem).
    
    *mudanças em torno dessa função nas classes relacionadas - 11/05/2022
    
    *Fazer tratamento de acessibilidade a epilepsia nos adapters que eu fiz até agora, fazer com que as imgView e o txtView que seja necessário ir para o perfil do usuário.
    
    * Implementar o recurso (responder comentário). Falta ocultação de comentário/responde comentários/colocar a função de denunciar  comentário, visibilidade da postagens, filtros e exibição para tal público na tela home ou amigos.
    
    -- Arrumar tela de filtros e ver se existe algum bug
    
    -- Terminar a função de editar publicação e configurar visibilidade da foto antes de exibir ela.(Público,privado,somente amigos,somente parceiros, amigos e parceiros, somente "fulano","ciclano")

--- app/src/main/java/com/example/ogima/adapter/AdapterComentarios.java
+++ app/src/main/java/com/example/ogima/adapter/AdapterComentarios.java
@@ -1,6 +1,9 @@
 package com.example.ogima.adapter;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,19 +13,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ogima.R;
 import com.example.ogima.helper.Base64Custom;
 import com.example.ogima.helper.ConfiguracaoFirebase;
 import com.example.ogima.helper.GlideCustomizado;
+import com.example.ogima.helper.ToastCustomizado;
 import com.example.ogima.model.Postagem;
 import com.example.ogima.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.iab.omid.library.giphy.adsession.video.Position;
 
 import java.util.Collections;
 import java.util.List;
@@ -52,7 +60,7 @@ public class AdapterComentarios extends RecyclerView.Adapter<AdapterComentarios.
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
 
 
 
@@ -73,6 +81,50 @@ public class AdapterComentarios extends RecyclerView.Adapter<AdapterComentarios.
                     holder.txtViewDataComentario.setText(postagemComentario.getDataComentario());
                     holder.txtViewComentario.setText(postagemComentario.getComentarioPostado());
 
+                    if(postagemComentario.getIdPostador().equals(idUsuarioLogado)){
+                        holder.imgButtonExcluirComentario.setVisibility(View.VISIBLE);
+                        holder.imgButtonExcluirComentario.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                AlertDialog.Builder builder =  new AlertDialog.Builder(view.getRootView().getContext());
+                                ProgressDialog progressDialog = new ProgressDialog(view.getRootView().getContext(), ProgressDialog.THEME_DEVICE_DEFAULT_DARK);
+                                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                                progressDialog.setMessage("Excluindo, por favor aguarde...");
+                                builder.setTitle("Deseja excluir seu comentário?");
+                                builder.setMessage("Seu comentário será excluído permanentemente");
+                                builder.setCancelable(true);
+                                builder.setPositiveButton("Excluir", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        progressDialog.show();
+                                        DatabaseReference excluirComentarioRef = firebaseRef.child("comentarios")
+                                                .child(postagemComentario.getIdPostagem()).child(idUsuarioLogado);
+                                        excluirComentarioRef.removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful()){
+                                                    progressDialog.dismiss();
+                                                    //Verificar se realmente está deixando a lista
+                                                    //em ordem correta ao remover dessa forma.
+                                                    listaComentarios.remove(position);
+                                                    notifyItemRemoved(position);
+                                                    ToastCustomizado.toastCustomizadoCurto("Comentário excluído com sucesso.", context.getApplicationContext());
+                                                }else{
+                                                    progressDialog.dismiss();
+                                                    ToastCustomizado.toastCustomizadoCurto("Erro ao excluir comentário, tente novamente!", context.getApplicationContext());
+                                                }
+                                            }
+                                        });
+                                    }
+                                });
+                                builder.setNegativeButton("Cancelar", null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        });
+                    }else{
+                        holder.imgButtonExcluirComentario.setVisibility(View.GONE);
+                    }
                 }
                 dadosUsuarioRef.removeEventListener(this);
             }
@@ -82,12 +134,6 @@ public class AdapterComentarios extends RecyclerView.Adapter<AdapterComentarios.
 
             }
         });
-
-        try{
-
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
     }
 
     @Override
@@ -101,7 +147,7 @@ public class AdapterComentarios extends RecyclerView.Adapter<AdapterComentarios.
         private TextView txtViewDataComentario, txtViewComentario,
                 txtViewTotalLikesComentario;
         private Button btnViewResponderComentario;
-        private ImageButton imgButtonLikeComentario;
+        private ImageButton imgButtonLikeComentario, imgButtonExcluirComentario;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -112,6 +158,7 @@ public class AdapterComentarios extends RecyclerView.Adapter<AdapterComentarios.
             txtViewTotalLikesComentario = itemView.findViewById(R.id.txtViewTotalLikesComentario);
             btnViewResponderComentario = itemView.findViewById(R.id.btnViewResponderComentario);
             imgButtonLikeComentario = itemView.findViewById(R.id.imgButtonLikeComentario);
+            imgButtonExcluirComentario = itemView.findViewById(R.id.imgButtonExcluirComentario);
         }
     }
 
