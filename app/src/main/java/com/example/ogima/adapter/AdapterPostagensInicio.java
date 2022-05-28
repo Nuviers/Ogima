c2f6b8166aa8475490497808fb1a172cc96c0c79
Rafae Benedet <rafabenedetfer@gmail.com>
2022-05-27 21:11:03 -0300

    * Recurso de curtir e descurtir postagem funcionando com sucesso, porém o botão fica sem função se clicado novamente, fazer algum jeito de notificar a mudança na lista.
    - Verificar com mais dados de usuários além de 1 único usuário para ver como se comporta, atualizar recycler no onStart caso seja possível.
    
    - Implementar o recurso de filtros e exibição para o público escolhido na tela home.
    
    -- Tipos de público que falta colocar(Combinações com parceiros + algum tipo de público ou Somente parceiros)

--- app/src/main/java/com/example/ogima/adapter/AdapterPostagensInicio.java
+++ app/src/main/java/com/example/ogima/adapter/AdapterPostagensInicio.java
@@ -1,7 +1,10 @@
 package com.example.ogima.adapter;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,25 +15,35 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ogima.R;
 import com.example.ogima.activity.PersonProfileActivity;
 import com.example.ogima.activity.TodasFotosUsuarioActivity;
+import com.example.ogima.fragment.InicioFragment;
 import com.example.ogima.helper.Base64Custom;
 import com.example.ogima.helper.ConfiguracaoFirebase;
 import com.example.ogima.helper.GlideCustomizado;
 import com.example.ogima.helper.ToastCustomizado;
 import com.example.ogima.model.Postagem;
 import com.example.ogima.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class AdapterPostagensInicio extends RecyclerView.Adapter<AdapterPostagensInicio.MyViewHolder> {
 
@@ -39,14 +52,18 @@ public class AdapterPostagensInicio extends RecyclerView.Adapter<AdapterPostagen
     private String idUsuarioLogado;
     private String emailUsuarioAtual;
     private DatabaseReference meusDadosRef, dadosSelecionadoRef;
-    private Postagem postagem;
     private Usuario usuarioAtual;
-    private Usuario usuarioSelecionado;
     private List<Postagem> listaFotosPostagens;
     private List<Usuario> listaUsuarioFotosPostagens;
     private Context context;
-    private String idUsuarioSelecionado;
 
+    private int contadorCurtidasPostagem;
+    private DatabaseReference verificaCurtidaRef,
+            adicionarCurtidaRef, caminhoCurtidaRef;
+    private String localUsuario;
+    private Locale localAtual;
+    private DateFormat dateFormat;
+    private Date date;
 
     public AdapterPostagensInicio(List<Postagem> listFotosPostagens, Context c, List<Usuario> listUsuarioFotosPostagens) {
         this.context = c;
@@ -54,6 +71,8 @@ public class AdapterPostagensInicio extends RecyclerView.Adapter<AdapterPostagen
         this.listaUsuarioFotosPostagens = listUsuarioFotosPostagens;
         emailUsuarioAtual = autenticacao.getCurrentUser().getEmail();
         idUsuarioLogado = Base64Custom.codificarBase64(emailUsuarioAtual);
+        localAtual = context.getResources().getConfiguration().locale;
+        localUsuario = localUsuario.valueOf(localAtual);
     }
 
     @NonNull
@@ -65,11 +84,142 @@ public class AdapterPostagensInicio extends RecyclerView.Adapter<AdapterPostagen
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
 
         try {
             Postagem postagemSelecionada = listaFotosPostagens.get(position);
             final Usuario[] usuarioSelecionado = {listaUsuarioFotosPostagens.get(position)};
+
+            //Verifica se usuário atual já curtiu essa postagem.
+            verificaCurtidaRef = firebaseRef.child("curtidasPostagem")
+                    .child(postagemSelecionada.getIdPostagem()).child(idUsuarioLogado);
+
+            verificaCurtidaRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.getValue() != null) {
+                        holder.imgButtonLikeFotoPostagemInicio.setImageResource(R.drawable.ic_heart_like_comentario_preenchido);
+                        holder.imgButtonLikeFotoPostagemInicio.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(view.getRootView().getContext());
+                                builder.setTitle("Desfazer curtida");
+                                builder.setMessage("Deseja remover sua curtida da postagem ?");
+                                builder.setPositiveButton("Remover curtida", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        DatabaseReference removerCurtidaRef = firebaseRef.child("curtidasPostagem")
+                                                .child(postagemSelecionada.getIdPostagem()).child(idUsuarioLogado);
+
+                                        removerCurtidaRef.removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    //Atualizando o contador de curtidas
+                                                    contadorCurtidasPostagem = postagemSelecionada.getTotalCurtidasPostagem() - 1;
+                                                    if(contadorCurtidasPostagem == -1){
+                                                        contadorCurtidasPostagem = 0;
+                                                    }
+                                                    DatabaseReference atualizarCurtidaRef = firebaseRef.child("postagensUsuario")
+                                                            .child(postagemSelecionada.getIdDonoPostagem())
+                                                            .child(postagemSelecionada.getIdPostagem())
+                                                            .child("totalCurtidasPostagem");
+                                                    atualizarCurtidaRef.setValue(contadorCurtidasPostagem).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            if(task.isSuccessful()){
+                                                                ToastCustomizado.toastCustomizadoCurto("Curtida removida com sucesso",context);
+                                                                holder.imgButtonLikeFotoPostagemInicio.setImageResource(R.drawable.ic_heart_like_comentario);
+                                                                holder.txtViewContadorLikesFotoPostagemInicio.setText("" + contadorCurtidasPostagem);
+
+                                                                holder.imgButtonLikeFotoPostagemInicio.setClickable(false);
+                                                            }else{
+                                                                ToastCustomizado.toastCustomizadoCurto("Erro ao remover curtida, tente novamente",context);
+                                                            }
+                                                        }
+                                                    });
+                                                }
+                                            }
+                                        });
+
+                                    }
+                                });
+                                builder.setNegativeButton("Cancelar", null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        });
+                    } else {
+                        holder.imgButtonLikeFotoPostagemInicio.setImageResource(R.drawable.ic_heart_like_comentario);
+                        holder.imgButtonLikeFotoPostagemInicio.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                //Setando os dados para salvar no caminho da curtidasPostagem
+                                HashMap<String, Object> dadosCurtida = new HashMap<>();
+                                if (localUsuario.equals("pt_BR")) {
+                                    dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                                    dateFormat.setTimeZone(TimeZone.getTimeZone("America/Sao_Paulo"));
+                                    date = new Date();
+                                    String novaData = dateFormat.format(date);
+                                    dadosCurtida.put("dataCurtidaPostagem", novaData);
+                                } else {
+                                    dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                                    dateFormat.setTimeZone(TimeZone.getTimeZone("America/Montreal"));
+                                    date = new Date();
+                                    String novaData = dateFormat.format(date);
+                                    dadosCurtida.put("dataCurtidaPostagem", novaData);
+                                }
+                                dadosCurtida.put("idPostagem", postagemSelecionada.getIdPostagem());
+                                dadosCurtida.put("idUsuarioInterativo", idUsuarioLogado);
+                                dadosCurtida.put("idDonoPostagem", postagemSelecionada.getIdDonoPostagem());
+
+                                caminhoCurtidaRef = firebaseRef.child("curtidasPostagem")
+                                        .child(postagemSelecionada.getIdPostagem()).child(idUsuarioLogado);
+
+                                caminhoCurtidaRef.setValue(dadosCurtida).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            //Verificando o total de curtidas da postagem
+                                            if (postagemSelecionada.getTotalCurtidasPostagem() > 0) {
+                                                contadorCurtidasPostagem = postagemSelecionada.getTotalCurtidasPostagem() + 1;
+                                            } else {
+                                                contadorCurtidasPostagem = 1;
+                                            }
+                                            //Atualizando o contador de curtidas
+                                            adicionarCurtidaRef = firebaseRef.child("postagensUsuario")
+                                                    .child(postagemSelecionada.getIdDonoPostagem())
+                                                    .child(postagemSelecionada.getIdPostagem())
+                                                    .child("totalCurtidasPostagem");
+                                            adicionarCurtidaRef.setValue(contadorCurtidasPostagem).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isSuccessful()) {
+                                                        ToastCustomizado.toastCustomizadoCurto("Curtido com sucesso", context);
+                                                        holder.imgButtonLikeFotoPostagemInicio.setImageResource(R.drawable.ic_heart_like_comentario_preenchido);
+                                                        holder.txtViewContadorLikesFotoPostagemInicio.setText("" + contadorCurtidasPostagem);
+
+                                                        holder.imgButtonLikeFotoPostagemInicio.setClickable(false);
+                                                    }else{
+                                                        ToastCustomizado.toastCustomizadoCurto("Erro ao curtir postagem, tente novamente", context);
+                                                    }
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                    }
+                    verificaCurtidaRef.removeEventListener(this);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
 
             //Referência dos dados atuais
             meusDadosRef = firebaseRef.child("usuarios").child(idUsuarioLogado);
@@ -113,7 +263,7 @@ public class AdapterPostagensInicio extends RecyclerView.Adapter<AdapterPostagen
                         if (postagemSelecionada.getTituloPostagem() != null && !postagemSelecionada.getTituloPostagem().equals("")) {
                             holder.txtViewTituloFotoPostadaInicio.setVisibility(View.VISIBLE);
                             holder.txtViewTituloFotoPostadaInicio.setText(postagemSelecionada.getTituloPostagem());
-                        }else{
+                        } else {
                             holder.txtViewTituloFotoPostadaInicio.setVisibility(View.GONE);
                         }
 
@@ -121,20 +271,20 @@ public class AdapterPostagensInicio extends RecyclerView.Adapter<AdapterPostagen
                         if (postagemSelecionada.getDescricaoPostagem() != null && !postagemSelecionada.getDescricaoPostagem().equals("")) {
                             holder.txtViewDescricaoFotoPostagemInicio.setVisibility(View.VISIBLE);
                             holder.txtViewDescricaoFotoPostagemInicio.setText(postagemSelecionada.getDescricaoPostagem());
-                        }else{
+                        } else {
                             holder.txtViewDescricaoFotoPostagemInicio.setVisibility(View.GONE);
                         }
 
                         //Exibindo o total de curtidas da postagem
                         if (postagemSelecionada.getTotalCurtidasPostagem() > 0) {
-                            holder.txtViewContadorLikesFotoPostagemInicio.setText(""+postagemSelecionada.getTotalCurtidasPostagem());
+                            holder.txtViewContadorLikesFotoPostagemInicio.setText("" + postagemSelecionada.getTotalCurtidasPostagem());
                         } else {
                             holder.txtViewContadorLikesFotoPostagemInicio.setText("0");
                         }
 
                         //Exibindo total de comentários da postagem
                         if (postagemSelecionada.getTotalComentarios() > 0) {
-                            holder.txtViewContadorComentarioFotoPostagemInicio.setText(""+postagemSelecionada.getTotalComentarios());
+                            holder.txtViewContadorComentarioFotoPostagemInicio.setText("" + postagemSelecionada.getTotalComentarios());
                         } else {
                             holder.txtViewContadorComentarioFotoPostagemInicio.setText("0");
                         }
@@ -160,7 +310,7 @@ public class AdapterPostagensInicio extends RecyclerView.Adapter<AdapterPostagen
             dadosSelecionadoRef.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if(snapshot.getValue() != null){
+                    if (snapshot.getValue() != null) {
                         usuarioSelecionado[0] = snapshot.getValue(Usuario.class);
                     }
                     dadosSelecionadoRef.removeEventListener(this);
