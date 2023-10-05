2e81a1c26ca278c642136b06d44f77f7460dadaa
Nuviers <rafabenedetfer@gmail.com>
2023-10-05 17:16:45 -0300

    Reajuste_final
     - LoginUiActivity otimizado.
     - LoginEmailActivity otimizado.
     - ViewCadastroActivity otimizado.
     - CadastroUserActivity otimizado.
     - CadastroEmailTermosActivity         otimizado.
     - ProblemasLogin otimizado.
     - PermissaoSegundoPlanoActivity otimizado
     - FotoPerfilActivity otimizado.
     - AlterarSenhaActivity otimizado.
    
     -* Activitys
     - QRCodeActivity otimizado.
     - OfflineActivity otimizado.
    
     * Lógica de foto e fundo de perfil, exclusão, upload, salvamento concluídos, falta ajustes como intent, onBackPressed e algumas alterações na lógica, também falta realizar mais alguns testes.
    
     * Ajustes na exclusão e bugs na edição de foto arrumados, strings configuradas para todo processo de cadastro, edição de dados comuns como nome, genero e interesses concluído, faltando apenas edição de perfil que não reflete ao cadastro.
    
     Concluído o processo de otimização na parte de cadastro de perfil.
    
     obs: Falta fazer o teste em todas telas que envolve número.
    
      - QRCode agora é mais seguro, ele utiliza um id único para ele sem comprometer a segurança do usuário.
    
    - 05/10/2023 - 17:16

--- app/src/main/java/com/example/ogima/activity/OfflineActivity.java
+++ app/src/main/java/com/example/ogima/activity/OfflineActivity.java
@@ -12,52 +12,62 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.ogima.R;
+import com.example.ogima.helper.ProgressBarUtils;
+import com.example.ogima.helper.ToastCustomizado;
 
 import java.io.IOException;
 
 public class OfflineActivity extends AppCompatActivity {
 
-    private Button buttonReconectar;
+    private Button btnReconectar;
     private ProgressBar progressBarOffline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offline);
+        inicializarComponentes();
+        clickListeners();
+    }
 
-        buttonReconectar = findViewById(R.id.buttonReconectar);
-        progressBarOffline = findViewById(R.id.progressBarOffline);
+    private void irParaSplashActivity(){
+        Intent intent = new Intent(getApplicationContext(), SplashActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
-        buttonReconectar.setOnClickListener(new View.OnClickListener() {
+    private boolean isConnected() throws InterruptedException, IOException {
+        String command = "ping -i 5 -c 1 google.com";
+        return Runtime.getRuntime().exec(command).waitFor() == 0;
+    }
+
+    private void clickListeners() {
+        btnReconectar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try{
-                    progressBarOffline.setVisibility(View.VISIBLE);
+                try {
+                    ProgressBarUtils.exibirProgressBar(progressBarOffline, OfflineActivity.this);
                     if (isConnected()) {
-                        progressBarOffline.setVisibility(View.GONE);
-                        Intent intent = new Intent(getApplicationContext(), SplashActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }else{
-                        progressBarOffline.setVisibility(View.GONE);
-                        String mensagemToast = "Sem conexão, por favor conecte seu wifi ou seus dados móveis e tente novamente";
+                        ProgressBarUtils.ocultarProgressBar(progressBarOffline, OfflineActivity.this);
+                        irParaSplashActivity();
+                    } else {
+                        ProgressBarUtils.ocultarProgressBar(progressBarOffline, OfflineActivity.this);
+                        String mensagemToast = getString(R.string.warning_no_connection);
                         SpannableStringBuilder biggerText = new SpannableStringBuilder(mensagemToast);
                         biggerText.setSpan(new RelativeSizeSpan(1.35f), 0, mensagemToast.length(), 0);
                         Toast.makeText(getApplicationContext(), biggerText, Toast.LENGTH_LONG).show();
                     }
-                }catch (Exception ex){
-                    progressBarOffline.setVisibility(View.GONE);
+                } catch (Exception ex) {
+                    ProgressBarUtils.ocultarProgressBar(progressBarOffline, OfflineActivity.this);
+                    ToastCustomizado.toastCustomizado(String.format("%s %s", getString(R.string.an_error_has_occurred), ex.getMessage()), getApplicationContext());
                     ex.printStackTrace();
                 }
             }
         });
-
-
-
     }
 
-    public boolean isConnected() throws InterruptedException, IOException {
-        String command = "ping -i 5 -c 1 google.com";
-        return Runtime.getRuntime().exec(command).waitFor() == 0;
+    private void inicializarComponentes() {
+        btnReconectar = findViewById(R.id.btnTentarReconectar);
+        progressBarOffline = findViewById(R.id.progressBarOffline);
     }
 }
